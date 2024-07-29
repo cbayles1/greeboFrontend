49401c26b6ba22e3187ff58f46f2d626fb19c913
Caleb <clbayles@hotmail.com>
2024-07-29 08:26:13 -0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  //templateUrl: './app.component.html',
+  template: `<p>{{sampleBar}}</p><br/> <button (click)="getChocBar()">Click Me!</button>`,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'frontend';
+  sampleBar: any = null;
+  baseUrl = "http://localhost:5099/api/";
+  constructor(private httpClient: HttpClient) {}
+  
+  getChocBar(): void {
+    this.httpClient.get(this.baseUrl + "chocolateBars/2", {headers: {Accept: 'application/json'}}).subscribe((res: any) => {
+      this.sampleBar = res;
+      console.log(this.sampleBar);
+    });
+  }
 }
